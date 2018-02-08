71b4ba8d46a28d8139f82c41381931cb7bb37447
Viet Hoang <viet.hoang@quant-edge.com>
2018-02-08 09:42:47 +0700

    Update PDFView.android.js

--- PDFView.android.js
+++ PDFView.android.js
@@ -1,7 +1,7 @@
 'use strict';
 import React,{ Component } from 'react';
 import { requireNativeComponent, View, ViewPropTypes} from 'react-native';
-
+const vPropTypes = ViewPropTypes || View.propTypes;
 class PDFView extends Component {
   constructor(props) {
     super(props);
@@ -22,13 +22,13 @@ class PDFView extends Component {
 }
 
 PDFView.propTypes = {
-  ...ViewPropTypes,
-    asset: ViewPropTypes.string,
-    src: ViewPropTypes.string,
-    pageNumber: ViewPropTypes.number,
-    path: ViewPropTypes.string,
-    zoom: ViewPropTypes.number,
-    onLoadComplete: ViewPropTypes.func
+  ...vPropTypes,
+    asset: vPropTypes.string,
+    src: vPropTypes.string,
+    pageNumber: vPropTypes.number,
+    path: vPropTypes.string,
+    zoom: vPropTypes.number,
+    onLoadComplete: vPropTypes.func
 };
 
 var PDFCustomView = requireNativeComponent('RCTPDFViewAndroid', PDFView, {
